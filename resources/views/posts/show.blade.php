c03f9587b30b9744e9a8d9d652a65c83c0efccb7
FHRONY <fhasanrony@gmail.com>
2019-12-11 22:31:40 +0600

    initial commit

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -45,6 +45,8 @@
 <br><br>
 <form action="{{ route('comments.store') }}" method="POST">
     @csrf
+    <?php  if(Auth::user())
+    { ?>
      <input type="hidden" name="user_id" value="{{Auth::user()->id}}">
       <input type="hidden" name="post_id" value="{{$post->id}}">
       <input type="hidden" name="user_name" value="{{Auth::user()->name}}">
@@ -58,6 +60,25 @@
               <button type="submit" class="btn btn-primary">Submit</button>
       </div>
   </div>
+<?php
+ }
+else
+{
+  ?>
+
+<div class="col-xs-12 col-sm-12 col-md-12">
+   <div class="form-group">
+       <strong>Post A Comment:</strong>
+       <textarea class="form-control" style="height:150px" name="user_comments" placeholder="Type Comment"></textarea>
+   </div>
+
+   <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+           <button type="submit" class="btn btn-primary">Submit</button>
+   </div>
+</div>
+<?php
+}
+?>
 
 </form>
 @endsection
